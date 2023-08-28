eba1aee0b41ca73b2beaec88927bc4071e1d0db0
Ian Githua <40959077+iangithua@users.noreply.github.com>
2023-08-28 12:50:33 +0300

    Cards Endpoint, Service and Repository

---
+++ src/main/java/com/logicea/cards/controller/CardController.java
@@ -0,0 +1,47 @@
+package com.logicea.cards.controller;
+
+import com.logicea.cards.entity.Card;
+import com.logicea.cards.service.CardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cards")
+public class CardController {
+
+    @Autowired
+    private CardService cardService;
+
+    @PostMapping
+    public ResponseEntity<Card> createCard(@RequestBody Card card) {
+        Card createdCard = cardService.createCard(card);
+        return new ResponseEntity<>(createdCard, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{cardId}")
+    public ResponseEntity<Card> getCardById(@PathVariable Long cardId) {
+        Card card = cardService.getCardById(cardId);
+        return new ResponseEntity<>(card, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Card>> getAllCards() {
+        List<Card> cards = cardService.getAllCards();
+        return new ResponseEntity<>(cards, HttpStatus.OK);
+    }
+
+    @PutMapping("/{cardId}")
+    public ResponseEntity<Card> updateCard(@PathVariable Long cardId, @RequestBody Card card) {
+        Card updatedCard = cardService.updateCard(cardId, card);
+        return new ResponseEntity<>(updatedCard, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{cardId}")
+    public ResponseEntity<Void> deleteCard(@PathVariable Long cardId) {
+        cardService.deleteCard(cardId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
---
+++ src/main/java/com/logicea/cards/service/CardService.java
@@ -0,0 +1,48 @@
+package com.logicea.cards.service;
+
+import com.logicea.cards.entity.Card;
+import com.logicea.cards.repository.CardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CardService {
+
+    @Autowired
+    private CardRepository cardRepository;
+
+    public Card createCard(Card card) {
+        // Additional validation and business logic can be added here
+        return cardRepository.save(card);
+    }
+
+    public Card getCardById(Long cardId) {
+        Optional<Card> optionalCard = cardRepository.findById(cardId);
+        return optionalCard.orElse(null);
+    }
+
+    public List<Card> getAllCards() {
+        return cardRepository.findAll();
+    }
+
+    public Card updateCard(Long cardId, Card card) {
+        Optional<Card> optionalCard = cardRepository.findById(cardId);
+        if (optionalCard.isPresent()) {
+            Card existingCard = optionalCard.get();
+            existingCard.setName(card.getName());
+            existingCard.setDescription(card.getDescription());
+            existingCard.setColor(card.getColor());
+            existingCard.setStatus(card.getStatus());
+            // Additional fields can be updated here
+            return cardRepository.save(existingCard);
+        }
+        return null;
+    }
+
+    public void deleteCard(Long cardId) {
+        cardRepository.deleteById(cardId);
+    }
+}
+
